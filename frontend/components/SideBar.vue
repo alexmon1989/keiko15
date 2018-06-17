c8a024356959270a5dee0a7408ae645fe48a9eb2
Alex Monastyretsky <alex.mon1989@gmail.com>
2018-06-17 20:23:40 +0300

    init commit

---
+++ frontend/components/SideBar.vue
@@ -0,0 +1,119 @@
+<template>
+  <b-tabs>
+    <b-tab title="Категории" active>
+      <div class="menu" v-bar>
+        <b-list-group>
+          <b-list-group-item href="#some-link">Awesome link</b-list-group-item>
+          <b-list-group-item href="#" active>Link with active state</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Last</b-list-group-item>
+        </b-list-group>
+      </div>
+    </b-tab>
+    <b-tab title="Ингредиенты" active>
+      <div class="menu" v-bar>
+        <b-list-group>
+          <b-list-group-item href="#some-link">Awesome link</b-list-group-item>
+          <b-list-group-item href="#" active>Link with active state</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Action links are easy</b-list-group-item>
+          <b-list-group-item href="#">Last</b-list-group-item>
+        </b-list-group>
+      </div>
+    </b-tab>
+  </b-tabs>
+
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss">
+  .menu {
+    height: calc(50vh - 4rem);
+
+    @media screen and (min-width: 768px) {
+      height: calc(100vh - 4rem);
+    }
+
+    .list-group-item {
+      background-color: #1e2021;
+      border-bottom: #2f3132 1px solid;
+    }
+  }
+
+
+
+</style>
---
+++ frontend/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <b-container fluid >
+      <b-row>
+        <b-col cols="12" md="3" xl="2" class="bd-sidebar">
+          <m-sidebar></m-sidebar>
+        </b-col>
+
+        <b-col cols="12" md="9" xl="8">
+          <nuxt/>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+
+<script>
+  import mSidebar from '~/components/SideBar.vue';
+
+  export default {
+    components: {mSidebar}
+  }
+</script>
